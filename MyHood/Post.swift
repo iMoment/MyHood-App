42caa3210d2b66585039d0156856e1a1462d3cb3
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-04 20:02:22 +0900

    Add Post class.

---
+++ MyHood/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  MyHood
+//
+//  Created by Stanley Pan on 2/4/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    private var imagePath: String!
+}
