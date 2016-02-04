7382951f2c573e123d3c44ff1ebc21a41265a77d
Stanley Pan <stanleypan.egre@gmail.com>
2016-02-04 22:39:34 +0900

    Add AddPostVC and layout in Storyboard

---
+++ MyHood/AddPostVC.swift
@@ -0,0 +1,18 @@
+//
+//  AddPostVC.swift
+//  MyHood
+//
+//  Created by Stanley Pan on 2/4/16.
+//  Copyright © 2016 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
